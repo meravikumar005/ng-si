4fefac7376f920f5d9cf3eb2ecad01103a598bca
meravikumar005 <meravikumar005@gmail.com>
2018-02-01 18:31:07 +0530

    initial commit

---
+++ src/app/pipe/si-suffix.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { SiSuffix } from "./si-suffix.pipe";
+
+@NgModule({
+  declarations:[
+    SiSuffix
+  ],
+  imports:[
+
+  ],
+  providers:[
+
+  ],
+  exports:[
+    SiSuffix
+  ]
+})
+
+export class SiSuffixModule {
+
+}
---
+++ src/app/pipe/si-suffix.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe , PipeTransform} from "@angular/core";
+declare let window:any;
+@Pipe({name:'siSuffix'})
+
+export class SiSuffix implements PipeTransform {
+  transform(value:number, decimal:number ) {
+    let suffixes = [ 'k', 'M', 'G', 'T', 'P', 'E'];
+    if(value === null || value === undefined) {
+      return null;
+    } else if (window.isNaN(value)) {
+      return null;
+    } else if(value < 1000) {
+      return value;
+    } else {
+      let exp = Math.floor(Math.log(value) / Math.log(1000));
+      return (value / Math.pow(1000,exp)).toFixed(2) + suffixes[exp -1];
+    }
+  }
+}
